692c6a0980e7b2bb47a89cc638b3f9afaf43ada2
lil-G <kahlil.rivera@yotpo.com>
2025-04-17 18:27:24 +0800

    star id

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   let InstanceId_QA = ""; 
   let InstanceId_SEO = "";
   let InstanceId_Carousel = "";
-  let InstanceId_StarRating = "1085246";
+  let InstanceId_StarRating = "1086873";
   let InstanceId_PromotedProd = ""; 
   let InstanceId_UgcGallery = ""; 
   let InstanceId_ReviewsTab = "";  
