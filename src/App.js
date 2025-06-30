ba2490c2126450dec8d2730fe6854e90bd0ed188
lil-G <kahlil.rivera@yotpo.com>
2025-07-01 05:56:45 +0800

    id appkey

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ import UgcGallery from "./components/UgcGallery";
 
 function App() { 
   //Instance ID a
-  let InstanceId_Widget = "1158034";  
+  let InstanceId_Widget = "1158060";  
   let InstanceId_QA = ""; 
   let InstanceId_SEO = "";
   let InstanceId_Carousel = "";
-  let InstanceId_StarRating = "1158053";
+  let InstanceId_StarRating = "1158068";
   let InstanceId_PromotedProd = ""; 
   let InstanceId_UgcGallery = ""; 
   let InstanceId_ReviewsTab = "";  
