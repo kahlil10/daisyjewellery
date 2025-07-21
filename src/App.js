0a9ad6b54a688a74a2fe37d5ca845448fa66e010
lil-G <kahlil.rivera@yotpo.com>
2025-07-22 04:38:46 +0800

    id

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   //Instance ID a
   let InstanceId_Widget = "380529";  
   let InstanceId_QA = "385088"; 
-  let InstanceId_SEO = ""; 
+  let InstanceId_SEO = "797399"; 
   let InstanceId_Carousel = "";
   let InstanceId_StarRating = "380528";
   let InstanceId_PromotedProd = ""; 
