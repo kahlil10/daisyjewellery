eae20681d4ce423cd160098655936ff26e77dd68
lil-G <kahlil.rivera@yotpo.com>
2025-07-30 01:02:58 +0800

    ggwp

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ import UgcGallery from "./components/UgcGallery";
 
 function App() { 
   //Instance ID a
-  let InstanceId_Widget = "1174630";  
-  let InstanceId_QA = "1174642"; 
+  let InstanceId_Widget = "1176995";  
+  let InstanceId_QA = "1177097"; 
   let InstanceId_SEO = ""; 
-  let InstanceId_Carousel = "1174645";
-  let InstanceId_StarRating = "1174643";
+  let InstanceId_Carousel = "1177099";
+  let InstanceId_StarRating = "1177096";
   let InstanceId_PromotedProd = ""; 
   let InstanceId_UgcGallery = ""; 
   let InstanceId_ReviewsTab = "";  
