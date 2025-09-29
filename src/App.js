a8d1bfdfa7ddbe4fefb0232e986ece60bc4e3da5
lil-G <kahlil.rivera@yotpo.com>
2025-09-29 14:12:04 +0800

    ggwp

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ import UgcGallery from "./components/UgcGallery";
 
 function App() { 
   //Instance ID a
-  let InstanceId_Widget = "832107";  
-  let InstanceId_QA = ""; 
+  let InstanceId_Widget = "";  
+  let InstanceId_QA = "832107"; 
   let InstanceId_SEO = ""; 
   let InstanceId_Carousel = "";
   let InstanceId_StarRating = "";
